aa3a67b647d0c62c5c06f4d6cd7cd7a753909f1b
joelwright-dev <proxy.coding@gmail.com>
2022-04-01 19:40:42 +1000

    Updated default page meta data

--- client/components/Meta.js
+++ client/components/Meta.js
@@ -6,7 +6,7 @@ const Meta = ({title, keywords, description}) => {
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <meta name="keywords" content={keywords}/>
         <meta name="description" content={description}/>
-        <meta charset="utf-8"/>
+        <meta charSet="utf-8"/>
         <link rel='icon' href='/favicon.ico'/>
         <title>{title}</title>
     </Head>
@@ -14,9 +14,9 @@ const Meta = ({title, keywords, description}) => {
 }
 
 Meta.defaultProps = {
-    title: "NextJS Test",
-    keywords: "Next, Next JS, React, JS, JavaScript",
-    description: "The best framework holy shit"
+    title: "Ultimatum",
+    keywords: "ultimatum, blockchain, contribution, development platform, open source, decentralized, decentralized autonomous organization, dao, organization",
+    description: "The open source organization empowering the community to make changes to the platforms they use. Our main goal is to provide a platform which allows users to contribute their ideas and skills and get rewarded for it."
 }
 
 export default Meta
