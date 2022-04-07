737bbf24e3254fb089aa40a6fee699a2fd348084
joelwright-dev <proxy.coding@gmail.com>
2022-04-08 09:03:00 +1000

    Success/error notification and redirect user once logged in

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,8 +1,12 @@
+import { Notification } from '@mantine/core'
+import { Check, X } from 'tabler-icons-react'
 import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 let users = require('../data/users.json')
 
 const Login = () => {
     const { query } = useRouter()
+    const router = useRouter()
 
     function publicKey() {
         if(typeof users.find(item => item.hash === query.ref) !== 'undefined') {
@@ -14,11 +18,25 @@ const Login = () => {
 
     function display(key) {
         if(typeof key === 'undefined') {
-            return("Invalid login")
+            return(
+                <Notification disallowClose icon={<X size={18}/>} color="red" title="Error!">
+                    Invalid URL    
+                </Notification>
+            )
         } else {
-            return(key)
+            return(
+                <Notification disallowClose icon={<Check size={18}/>} color="teal" title="Success!">
+                    Successfully signed in on public key: {key}
+                </Notification>
+            )
         }
     }
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/')
+        }, 3000)
+    }, [])
 
     return(
         <div>
